76906f153eb852345e83b00e48e3899fea6cdc1b
Geoff Maciolek <GeoffMaciolek@gmail.com>
2017-01-23 16:59:57 -0500

    Write out some files

--- secure-log-ordered-display.sh
+++ secure-log-ordered-display.sh
@@ -4,23 +4,27 @@
 # Finds the last login times (within reason) of SFTP users
 #
 
-SearchDir="$HOME/logtemp/short" #"/var/log"  # this is where we look for the file(s)
+### "User" variables - set these as needed
+
+SearchDir="$HOME/logtemp/full" #"/var/log"  # this is where we look for the file(s)
 fNamePattern='secure*' # Filenames to look for
 searchPattern='session opened for user [pP][kK][0-9]' # Search for lines containing this
 excludePattern="We Aren't Using This But Don't Leave It Empty" # What to exclude
+debugLogging=false # set to "true" or "false" without quotes.  Note that this line is EXECUTED by the if statements - be careful
+
+#### Begin Script ####
 
 LogTempFile=$(mktemp)
 FileList=$(find "$SearchDir" -type f -name "$fNamePattern" -printf '%TY-%Tm-%Td %p\n'|sort)
 
-
-echo $FileList
+if $debugLogging; then echo "File list: $FileList"; fi 
 
 # Iterate through the above list
-while read -r line; do
-  FileName=$(echo "$line"|cut -f2 -d' ')
-  FileYear=$(echo "$line"|cut -f1 -d'-')
-  FileMonth=$(echo "$line"|cut -f2 -d'-')
-  echo "The file YEAR is $FileYear and the FILE is $FileName in month $FileMonth Line is: ${line}"
+while read -r currentFile; do
+  FileName=$(echo "$currentFile"|cut -f2 -d' ') # The line is formatted e.g. "2016-07 /var/log/secure-20160813"
+  FileYear=$(echo "$currentFile"|cut -f1 -d'-') # So we extract these variables from the line content
+  FileMonth=$(echo "$currentFile"|cut -f2 -d'-')
+  if $debugLogging ; then echo "File Timestamp: ${FileYear}-${FileMonth} FileName: $FileName Line is: ${currentFile}"; fi
 
    #grep -E "$searchPattern" "$FileName"| grep -vE "$excludePattern")
   while IFS= read -r logline; do
@@ -29,13 +33,15 @@ while read -r line; do
       MatchLine="$logline"
 #      echo "MATCH: $logline"
       # Clean the log line to enable reading - only get the "text" month, day, time, and the login user
-      cleanLogLine=$(echo "$logline"| cut -d' ' -f1,2,3,11)
-      read LineMonTxt LineDay LineTime LineUser <<< "$logline" # Extract the date/time from the line
+      cleanLogLine=$(echo "$logline"|tr -s ' '|cut -d' ' -f1,2,3,11) # the "tr -s" removes multiple concurrent spaces, e,g "Jan  1" which confuse "cut"
+      read LineMonTxt rawLineDay LineTime LineUser <<< "$cleanLogLine" # Extract the date/time from the line
+      LineDay=$(printf "%02d" $rawLineDay)
       LineMon=$(date -d "$LineMonTxt 01" "+%m") # Convert the three letter month to a leading-zero number
 
 #      LineMonth=$(echo "$logline"|cut -d' ' -f1)
       # If the file is 'early' in the year, and the log line 'Late' month, we may need to fix the year info by subtracting
       if [[ ($FileMonth -lt 5) && ($LineMon -gt 7) ]]; then   # one from Year (as the file in Jan may have lines from Dec, etc)
+         if $debugLogging; then echo "FIXING THIS ONE: ($currentFile): $logline"; fi
          LineYear=$(($FileYear - 1))
       else
          LineYear=$FileYear
@@ -45,7 +51,7 @@ while read -r line; do
        # LogLine looks like: Jan 20 11:48:59 storage01 sshd[1891]: pam_unix(sshd:session): session opened for user pk11328 by (uid=0)
 
 #      formattedLogLine=$logline
-#      echo "$formattedLogLine"
+#      echo "$formattedLogLine XX $logline"
       echo "$formattedLogLine" >> "$LogTempFile"
 #    else
 #       echo "X: $logline"
@@ -57,13 +63,15 @@ done <<< "$FileList"
 #exit
 
 UserlistTempFile=$(mktemp)
-lslogins -o USER|grep -E '^pk[0-9]' > "$UserlistTempFile"
-Column=2 #This is the position in the log that contains the actual username - space delimited
-awk 'FNR==NR{ user[$1]; next } $'$Column' in user { lastline[$'$Column'] = $0 } END { for (u in lastline) print u": "lastline[u]}' "$UserlistTempFile" "$LogTempFile"
+lslogins -o USER|grep -E '^pk[0-9]' |sort -V > "$UserlistTempFile" # "sort -V" is 'version' sorting; works for fidning pk56 smaller than pk103
+Column=3 #This is the position in the log that contains the actual username - space delimited
+#awk 'FNR==NR{ user[$1]; next } $'$Column' in user { lastline[$'$Column'] = $0 } END { for (u in lastline) print u": "lastline[u]}' "$UserlistTempFile" "$LogTempFile"
+LastLoginsUnsorted=$(awk 'FNR==NR{ user[$1]; next } $'$Column' in user { lastline[$'$Column'] = $0 } END { for (u in lastline) print lastline[u]}' "$UserlistTempFile" "$LogTempFile")
+LastLoginsDateSorted=$(echo "$LastLoginsUnsorted" | sort --stable --key=1,2) # Sort by the timestamp
+LastLoginsUsernameSorted=$(echo "$LastLoginsUnsorted" | sort --key=3 -V) # Sort by the username (see above for -V expl.)
 
-
-
-
+echo "$LastLoginsDateSorted" > datesorted.txt
+echo "$LastLoginsUsernameSorted" > usersorted.txt
 
 rm "$UserlistTempFile" "$LogTempFile"
 
